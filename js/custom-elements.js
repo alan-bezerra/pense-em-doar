c1378878442ff52ac3058492b0cad096f0cf6e63
Raiane Oliveira <raiane.oliveira404@gmail.com>
2025-04-07 17:47:56 -0300

    feat: pagina comunidade

--- js/custom-elements.js
+++ js/custom-elements.js
@@ -23,6 +23,38 @@ const nivelUrgenciaIcons = {
   baixo: '<i class="ph ph-battery-vertical-low"></i>',
 }
 
+class NavBar extends HTMLElement {
+  connectedCallback() {
+    const active = this.dataset.active // home, pedidos, comunidade, onde-doar
+
+    this.innerHTML = `
+      <div class="app-navbar">
+        <nav class="container">
+          <a href="/" class="nav-link">
+            <i class="${active === "home" ? "ph-fill" : "ph"} ph-house"></i>
+            <span>Home</span>
+          </a>
+
+          <a href="/pedidos.html" class="nav-link">
+            <i class="${active === "pedidos" ? "ph-fill" : "ph"} ph-hand-heart"></i>
+            <span>Pedidos</span>
+          </a>
+
+          <a href="/comunidade.html" class="nav-link">
+            <i class="${active === "comunidade" ? "ph-fill" : "ph"} ph-users-three"></i>
+            <span>Comunidade</span>
+          </a>
+
+          <a href="/onde-doar.html" class="nav-link">
+            <i class="${active === "onde-doar" ? "ph-fill" : "ph"} ph-map-trifold"></i>
+            <span>Onde doar</span>
+          </a>
+        </nav>
+      </div>
+`
+  }
+}
+
 class PedidoDoacaoCard extends HTMLElement {
   connectedCallback() {
     const { 
@@ -36,7 +68,7 @@ class PedidoDoacaoCard extends HTMLElement {
       nivel, // urgente, medio, baixo
     } = this.dataset
 
-    const id = Math.floor(Math.random() * 1000)
+    const id = Math.floor(Math.random() * 1000) // NOTE: Viria do backend
     const precisaAte = precisaAteValue ?? "Indefinido"
     const fullAddress = `${endereco} - ${cidade} / ${uf.toUpperCase()}`
 
@@ -142,36 +174,124 @@ class PedidoDoacaoCard extends HTMLElement {
   }
 }
 
-class NavBar extends HTMLElement {
+class ComunidadeDoacaoCard extends HTMLElement {
   connectedCallback() {
+    const { 
+      paciente, 
+      autor,
+      sangues, 
+      compativel,
+      endereco, 
+      postadoEm,
+      comentarios,
+    } = this.dataset
+
+    const iniciaisAutor = autor.length > 1 ? autor.slice(0, 2) : autor.slice(0, 1)
+    const sanguesArr = sangues.split(",").filter((sangue) => sangue.split().length > 0)
+    const isCompativel = Boolean(compativel)
+
     this.innerHTML = `
-      <div class="app-navbar">
-        <nav class="container">
-          <a href="/" class="nav-link">
-            <i class="ph-fill ph-house"></i>
-            <span>Home</span>
-          </a>
+      <article class="comunidade-post-card">
+        <header class="card-author-header">
+          <div class="d-flex align-items-center gap-2">
+            <div class="avatar-card flex-shrink-0">
+              <span class="avatar-placeholder">${iniciaisAutor}</span>
+            </div>
 
-          <a href="/pedidos.html" class="nav-link">
-            <i class="ph ph-hand-heart"></i>
-            <span>Pedidos</span>
-          </a>
+            <p class="card-author-title">
+              <span>${autor}</span> fez um pedido de doação
+            </p>
+          </div>
 
-          <a href="/comunidade.html" class="nav-link">
-            <i class="ph ph-users-three"></i>
-            <span>Comunidade</span>
-          </a>
+          <div class="dropdown">
+            <button
+              class="btn"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <i class="ph ph-dots-three-vertical"></i>
+            </button>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Compartilhar</a></li>
+              <li><a class="dropdown-item" href="#">Baixar imagem</a></li>
+            </ul>
+          </div>
+        </header>
 
-          <a href="/pedidos.html" class="nav-link">
-            <i class="ph ph-map-trifold"></i>
-            <span>Onde doar</span>
-          </a>
-        </nav>
-      </div>
-`
+        <div class="card-body-container">
+          <section class="card-body">
+            <h6 class="card-body-title">Pedido de doação</h6>
+            <ul class="card-body-list">
+              <li class="card-list-item">
+                <div class="card-icon-text">
+                  <i class="ph ph-user"></i>
+                  <span class="card-text">PACIENTE</span>
+                </div>
+
+                <p class="card-list-description">${paciente}</p>
+              </li>
+
+              <li class="card-list-item">
+                <div class="card-icon-text">
+                  <i class="ph ph-drop"></i>
+                  <span class="card-text">SANGUE NECESSÁRIO</span>
+                </div>
+
+                <div class="card-list-horizontal">
+                  ${sanguesArr.map((sangue) => `<span class="badge">${sangue}</span>`).join('\n')}
+                </div>
+
+                ${isCompativel ? `
+                  <p
+                    class="card-list-description card-list-description--detail"
+                  >
+                    Você é compatível.
+                  </p>
+                ` : ""}
+              </li>
+
+              <li>
+                <a href="/onde-doar.html" class="card-list-item">
+                  <div
+                    class="d-flex align-items-center justify-content-between"
+                  >
+                    <div class="card-icon-text">
+                      <i class="ph ph-map-pin"></i>
+                      <span class="card-text">ONDE DOAR</span>
+                    </div>
+
+                    <i class="ph ph-caret-right"></i>
+                  </div>
+
+                  <p class="card-list-description">${endereco}</p>
+                </a>
+              </li>
+            </ul>
+          </section>
+
+          <footer class="card-footer">
+            <div class="card-icon-text card-icon-text--sm">
+              <i class="ph-fill ph-globe-hemisphere-west"></i>
+              <span
+                class="card-list-description card-list-description--detail"
+              >
+                ${postadoEm}
+              </span>
+            </div>
+
+            <div class="card-icon-text card-comment">
+              <i class="ph ph-chat"></i>
+              <span>${comentarios}</span>
+            </div>
+          </footer>
+        </div>
+      </article>
+    `
   }
 }
 
 window.customElements.define('app-header', Header)
 window.customElements.define('app-navbar', NavBar)
 window.customElements.define('pedido-doacao-card', PedidoDoacaoCard)
+window.customElements.define('comunidade-post-card', ComunidadeDoacaoCard)
